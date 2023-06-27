6270f8859bad3deb6bf193bca79338f3d2e42f24
unknown <milagos31@gmail.com>
2023-06-27 21:51:02 +0800

    added Login in pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,43 +8,45 @@ import Root from "./Root";
 import Product from "./pages/Product";
 import About from "./pages/About";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "home",
-        element: <Home />,
-      },
-      {
-        path: "product",
-        element: <Product />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-},{path: 'login', element:<h1>Login</h1>}
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "contact",
+                element: <Contact />,
+            },
+            {
+                path: "home",
+                element: <Home />,
+            },
+            {
+                path: "product",
+                element: <Product />,
+            },
+            {
+                path: "about",
+                element: <About />,
+            },
+        ],
+    },
+    { path: "login", element: <Login /> },
 ]);
 
 function App() {
-  return (
-    <div className="App">
-      <div className="nav-bar">
-        {/* <Home /> */}
-        {/* <NavNames /> */}
-        <RouterProvider router={router} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="nav-bar">
+                {/* <Home /> */}
+                {/* <NavNames /> */}
+                <RouterProvider router={router} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
