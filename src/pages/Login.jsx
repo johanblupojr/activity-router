b32b6668e0174fde7402be7c69f48b52adb83d7f
unknown <milagos31@gmail.com>
2023-06-29 17:41:42 +0800

    added login UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,23 @@
+import "./Login.css";
+
 export default function Login() {
-    return <h1>Login</h1>;
+    return (
+        <>
+            <form>
+                <div className="form-field">
+                    <input type="email" placeholder="Email / Username" required />
+                </div>
+
+                <div className="form-field">
+                    <input type="password" placeholder="Password" required />
+                </div>
+
+                <div className="form-field">
+                    <button className="btn" type="submit">
+                        Log in
+                    </button>
+                </div>
+            </form>
+        </>
+    );
 }
