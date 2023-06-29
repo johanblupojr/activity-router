1555aa254148b4fe6fb2e646f9a06a970738d0d3
unknown <milagos31@gmail.com>
2023-06-29 17:32:53 +0800

    added login in nav

--- src/components/NavNames.jsx
+++ src/components/NavNames.jsx
@@ -1,44 +1,51 @@
 import { Link } from "react-router-dom";
 
-const logo =  '/src/images/logo.png'
+const logo = "/src/images/logo.png";
 function NavNames() {
     const data = [
         {
-            'nav': 'Home',
-            'link': 'home'
+            nav: "Home",
+            link: "home",
         },
         {
-            'nav': 'About',
-            'link': 'about'
+            nav: "About",
+            link: "about",
         },
         {
-            'nav': 'Product',
-            'link': 'product'
+            nav: "Product",
+            link: "product",
         },
         {
-            'nav': 'Contact',
-            'link': 'contact'
-        }
-    ]
-    const listItems = data.map((element,index) => {
+            nav: "Contact",
+            link: "contact",
+        },
+        {
+            nav: "Login",
+            link: "login",
+        },
+    ];
+    const listItems = data.map((element, index) => {
         return (
-            <ul key={index} style = {{listStyle: 'none'}}>
-                <li><Link to={element.link} className='list'>{element.nav}</Link></li>
+            <ul key={index} style={{ listStyle: "none" }}>
+                <li>
+                    <Link to={element.link} className="list">
+                        {element.nav}
+                    </Link>
+                </li>
             </ul>
-        )
-    }
-    )
+        );
+    });
     return (
-        <div className='navigation'>
-            <div className='title'><img className='logo' src={logo} alt="" />
-            <h2>Salford & Co.</h2>
+        <div className="navigation">
+            <div className="title">
+                <img className="logo" src={logo} alt="" />
+                <h2>Salford & Co.</h2>
             </div>
-            <div className='nav-items' style = {{listStyle: 'none'}}>
-            {listItems}
+            <div className="nav-items" style={{ listStyle: "none" }}>
+                {listItems}
             </div>
-            
         </div>
-    )
+    );
 }
 
-export default NavNames
+export default NavNames;
