2b245edab170659d7516563d3e728b2eddad909f
unknown <milagos31@gmail.com>
2023-06-29 18:22:48 +0800

    fixed bug in css login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,21 +3,23 @@ import "./Login.css";
 export default function Login() {
     return (
         <>
-            <form>
-                <div className="form-field">
-                    <input type="email" placeholder="Email / Username" required />
-                </div>
+            <div className="login">
+                <form>
+                    <div className="form-field">
+                        <input type="email" placeholder="Email / Username" required />
+                    </div>
 
-                <div className="form-field">
-                    <input type="password" placeholder="Password" required />
-                </div>
+                    <div className="form-field">
+                        <input type="password" placeholder="Password" required />
+                    </div>
 
-                <div className="form-field">
-                    <button className="btn" type="submit">
-                        Log in
-                    </button>
-                </div>
-            </form>
+                    <div className="form-field">
+                        <button className="btn" type="submit">
+                            Log in
+                        </button>
+                    </div>
+                </form>
+            </div>
         </>
     );
 }
