2b7972d7438095c0bacf529336be2e05850a6cd0
johanblupojr <johanblupojr@gmail.com>
2023-07-10 01:33:40 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./styles.css";
 import Home from "./pages/Home";
 import NavNames from "./components/NavNames";
@@ -7,46 +8,28 @@ import Contact from "./pages/Contact";
 import Root from "./Root";
 import Product from "./pages/Product";
 import About from "./pages/About";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/Login";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "contact",
-                element: <Contact />,
-            },
-            {
-                path: "home",
-                element: <Home />,
-            },
-            {
-                path: "product",
-                element: <Product />,
-            },
-            {
-                path: "about",
-                element: <About />,
-            },
-        ],
-    },
-    { path: "login", element: <Login /> },
-]);
-
 function App() {
-    return (
-        <div className="App">
-            <div className="nav-bar">
-                {/* <Home /> */}
-                {/* <NavNames /> */}
-                <RouterProvider router={router} />
-            </div>
+  return (
+    <div className="App">
+      <Router>
+        <div className="nav-bar">
+          {/* <Home /> */}
+          {/* <NavNames /> */}
+          <Switch>
+            <Route exact path="/activity-router" component={Root} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/home" component={Home} />
+            <Route path="/product" component={Product} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route component={ErrorPage} />
+          </Switch>
         </div>
-    );
+      </Router>
+    </div>
+  );
 }
 
 export default App;
