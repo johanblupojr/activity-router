66e9b7e7f60c240251e9071eeedfe0ca3104cb62
johanblupojr <johanblupojr@gmail.com>
2023-07-10 02:03:58 +0800

    commit

--- src/components/messenger1.jsx
+++ src/components/messenger1.jsx
@@ -1,29 +1,49 @@
 import React, { useEffect } from "react";
 
-const FacebookMessengerChat1 = () => {
+const FacebookMessengerChat = () => {
   useEffect(() => {
-    window.fbAsyncInit = function () {
-      FB.init({
-        xfbml: true,
-        version: 'v17.0'
-      });
-    };
+    const script = document.createElement("script");
+    script.innerHTML = `
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "108965818922829");
+      chatbox.setAttribute("attribution", "biz_inbox");
 
-    (function (d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml: true,
+          version: 'v17.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    `;
+    document.body.appendChild(script);
   }, []);
 
   return (
     <>
       <div id="fb-root"></div>
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <div
+        id="fb-customer-chat"
+        className="fb-customerchat"
+        dangerouslySetInnerHTML={{
+          __html: `
+            <div id="fb-root"></div>
+            <div class="fb-customerchat"
+              attribution="biz_inbox"
+              page_id="108965818922829"
+            ></div>
+          `,
+        }}
+      ></div>
     </>
   );
 };
 
-export default FacebookMessengerChat1;
+export default FacebookMessengerChat;
