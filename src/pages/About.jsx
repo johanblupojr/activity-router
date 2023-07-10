6b065defa6bda1614f3dbf158a6550615212791d
johanblupojr <johanblupojr@gmail.com>
2023-07-10 11:16:09 +0800

    Commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import FacebookMessengerChatTwo from "../components/messenger2";
 export default function About(){
     return(
         <>
-        <FacebookMessengerChatTwo/>
         <h1>ABOUT PAGE</h1>
+        <FacebookMessengerChatTwo/>
         </>
 
     )
